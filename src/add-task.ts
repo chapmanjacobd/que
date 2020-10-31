f3fcc56946c82a3b9feeb3f3f54fbd696ebf9407
xk3 <7908073+chapmanjacobd@users.noreply.github.com>
2020-10-31 17:26:05 -0600

    wip

--- src/add-task.ts
+++ src/add-task.ts
@@ -1,39 +1,61 @@
-import shelf from "node-persist";
 import { config } from "./config";
 import { Task } from "./types";
 import sqlite from "better-sqlite3";
+import { join } from "path";
 
-const defaultTaskObject = (taskCmd: string): Task => {
-  return { taskCmd, status: "QUEUED" };
+const taskTableName = `${config.queueName}_tasks`;
+
+const defaultTaskObject = (taskCmd?: string): Task => {
+  if (taskCmd) return { taskCmd, status: "QUEUED" };
+  return;
 };
-
-// disregard add task if the same command already exists in the queue (whether or not it exited successfully or not)
-const atomic = process.env.ADD_TASK_STYLE === "ATOMIC";
-
-export async function addTask(newTaskCmd?: Task): Promise<Task[]> {
-  await shelf.init({ dir: config.queueName });
-
-  const cart: Task[] = (await shelf.getItem("taskList")) ?? [];
-
-  if (!newTaskCmd) return cart;
-
-  const db = sqlite("foobar.db");
-
-  const row = db.prepare(`SELECT * FROM ${} WHERE taskCmd = ${}`).pluck();
-  console.log(row.firstName, row.lastName, row.email);
-
-  const taskList =
-    atomic && cart.find((known) => newTaskCmd.taskCmd === known.taskCmd)
-      ? cart.map((known) => (newTaskCmd.taskCmd === known.taskCmd ? known : newTaskCmd))
-      : [...cart, newTaskCmd];
-
-  await shelf.setItem("taskList", taskList);
-
-  return taskList;
-}
 
 if (require.main === module)
   (async () => {
     const cmd = defaultTaskObject(process.argv[2]);
-    console.log(await addTask(cmd));
+    console.log(addTask(cmd));
   })();
+
+export function addTask(newTaskCmd?: Task): Task[] {
+  const db = sqlite(join(__dirname, "..", "db.sqlite"));
+
+  makeSureTablesExist(db);
+  makeSureQueueExists(db);
+  if (newTaskCmd) insertTask(newTaskCmd, db);
+
+  return db.prepare(`SELECT * FROM ${taskTableName} WHERE status != 'FINISHED'`).all();
+}
+
+function insertTask(newTaskCmd: Task, db: sqlite.Database) {
+  db.prepare(`INSERT INTO ${taskTableName} VALUES (@task_cmd, @status)`).run({
+    task_cmd: newTaskCmd.taskCmd,
+    status: newTaskCmd.status,
+  });
+}
+
+function makeSureQueueExists(db: sqlite.Database) {
+  const queueExists = db.prepare(`SELECT * FROM queues WHERE q_name = '${config.queueName}'`).get();
+
+  if (!queueExists) {
+    db.prepare(`INSERT INTO queues VALUES (@q_name, @status)`).run({
+      q_name: config.queueName,
+      status: "RUNNING",
+    });
+  }
+}
+
+function makeSureTablesExist(db: sqlite.Database) {
+  db.prepare(
+    `CREATE TABLE IF NOT EXISTS queues (
+      q_name TEXT NOT NULL PRIMARY KEY,
+      status TEXT NOT NULL
+    )`
+  ).run();
+
+  db.prepare(
+    `CREATE TABLE IF NOT EXISTS ${taskTableName} (
+      task_cmd TEXT NOT NULL,
+      status TEXT NOT NULL
+    )`
+  ).run();
+}
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const config = { queueName: process.argv[3] ?? "default-queue" };
+export const config = { queueName: process.argv[3] ?? "default_queue" };
