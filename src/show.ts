312d6904e0111063f0e7aabade0a730e20ed0e9a
xk3 <7908073+chapmanjacobd@users.noreply.github.com>
2020-11-05 20:53:37 -0600

    wip

--- src/show.ts
+++ src/show.ts
@@ -28,8 +28,7 @@ export function showTasks() {
   if (process.argv.includes("json"))
     return console.log({ queuedTasks, runningTasks, failedTasks, completeTasks });
 
-  // console.log("Queued Tasks:");
-  // console.table(queuedTasks.map((t) => truncateTask(t)));
+  console.log("Queued Tasks:", queuedTasks.length);
 
   console.log("Running Tasks:");
   console.table(runningTasks.map((t) => truncateTask(t)));
